4ab01c77a8d85fd9811b343bee1d6c8d42c2e288
icobx <jakub.fedorko@icloud.com>
2021-06-22 00:06:31 +0200

    carousel transition fix and new styling

--- src/components/ProjectCarousel.vue
+++ src/components/ProjectCarousel.vue
@@ -1,36 +1,40 @@
 <template>
 	<div class="carousel-wrapper">
-		<button id="previous" class="carousel-btn" @click="swipe(true)">&lt;</button>
-		<div class="carousel-cards">
-			<transition-group name="slide" mode="out-in">
-				<project-card
-					v-for="project in projects"
-					:key="project.name"
-					:project="project"
-					v-show="project.active"
-					class="carousel-card"
-				/>
-			</transition-group>
-			<!-- :class="{ hidden: !project.active }" -->
-		</div>
-		<button id="next" class="carousel-btn" @click="swipe(false)">&gt;</button>
+		<a id="previous" class="carousel-btn" @click="swipe(true)"
+			><font-awesome-icon icon="chevron-left" class="carousel-btn-icon"
+		/></a>
+		<transition-group tag="div" class="carousel-cards" :name="transitionName" mode="out-in">
+			<project-card
+				v-for="project in projects"
+				:key="project.name"
+				:project="project"
+				v-show="project.active"
+				class="carousel-card"
+			/>
+		</transition-group>
+		<a id="next" class="carousel-btn" @click="swipe(false)"
+			><font-awesome-icon icon="chevron-right" class="carousel-btn-icon"
+		/></a>
 	</div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, ref } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-// import Slick from 'vue-slick'
 import ProjectCard from './ProjectCard.vue'
 import ProjectData from '@/types/ProjectData'
-import 'slick-carousel/slick/slick.css'
 
 export default defineComponent({
 	name: 'ProjectCarousel',
 	components: {
+		FontAwesomeIcon,
 		ProjectCard,
 	},
 	setup() {
+		library.add(faChevronRight, faChevronLeft)
 		const projects = ref<ProjectData[]>([
 			{
 				name: 'project 1',
@@ -55,12 +59,17 @@ export default defineComponent({
 			},
 		])
 
+		const transitionName = ref('slide-next')
+
 		const activeProjects = computed(() => {
 			return projects.value.filter((project) => project.active)
 		})
 
 		const swipe = (prev: boolean) => {
 			let n = projects.value.length
+
+			if (prev) transitionName.value = 'slide-prev'
+			else transitionName.value = 'slide-next'
 
 			for (let i = 0; i < n; i++) {
 				if (projects.value[i].active) {
@@ -72,7 +81,7 @@ export default defineComponent({
 			}
 		}
 
-		return { projects, activeProjects, swipe }
+		return { projects, activeProjects, swipe, transitionName }
 	},
 })
 </script>
@@ -81,7 +90,7 @@ export default defineComponent({
 .carousel {
 	&-wrapper {
 		margin: 8em 0;
-		border: 1px solid red;
+		// border: 1px solid red;
 		max-height: 50vh;
 		display: flex;
 		justify-content: space-between;
@@ -101,35 +110,78 @@ export default defineComponent({
 		position: absolute;
 		left: 0;
 	}
+
+	&-btn {
+		text-transform: uppercase;
+		background-color: transparent;
+		color: white;
+		padding: 0 2em;
+		border: 0;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		&-icon {
+			// border: 1px solid white;
+			width: 1em;
+			height: 1em;
+		}
+		&:hover {
+			background-color: rgba($color: #000000, $alpha: 0.3);
+			// border: 1px solid white;
+			// border-width: 1px 0 1px 1px;
+		}
+		// &:first-child {
+
+		// }
+
+		// &:last-child {
+		// 	&:hover {
+		// 		// border: 1px solid white;
+		// 		// border-width: 1px 1px 1px 0;
+		// 	}
+		// }
+	}
 }
+
 .slide {
-	&-enter {
-		opacity: 0;
-		// right: 30vw;
-		transform: translateX(100px);
-
-		&-to {
+	&-prev,
+	&-next {
+		&-enter-to,
+		&-leave-from {
 			opacity: 1;
-			// right: 0;
+			left: 0;
+		}
+
+		&-enter-active {
+			transition: all 0.5s ease-in-out 0.5s;
+		}
+		&-leave-active {
+			transition: all 0.5s ease-in-out;
 		}
 	}
 
-	&-leave {
-		opacity: 1;
-		// left: 0;
-
-		&-to {
+	&-prev {
+		&-enter-from {
 			opacity: 0;
-			// left: 30vw;
-			transform: translateX(100px);
+			left: 150px;
+		}
+		&-leave-to {
+			opacity: 0;
+			left: -150px;
 		}
 	}
 
-	&-enter-active {
-		transition: all 0.5 ease-in;
-	}
-	&-leave-active {
-		transition: all 0.5s ease-out;
+	&-next {
+		&-enter-from {
+			opacity: 0;
+			left: -150px;
+		}
+		&-leave-to {
+			opacity: 0;
+			left: 150px;
+		}
 	}
 }
 .hidden {
